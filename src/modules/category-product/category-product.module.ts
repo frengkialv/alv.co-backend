96e93eab985aa849861554fc35f7223da8f71e87
unknown <and.frengky@gmail.com>
2024-09-08 00:20:06 +0700

    add entity category product

--- src/modules/category-product/category-product.module.ts
+++ src/modules/category-product/category-product.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CategoryProductService } from './category-product.service';
 import { CategoryProductController } from './category-product.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryProductEntity } from './entities/category-product.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CategoryProductEntity])],
   controllers: [CategoryProductController],
   providers: [CategoryProductService],
 })
