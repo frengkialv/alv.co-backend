362294de4cdbabd83cc35af87e094b5c674471b3
unknown <and.frengky@gmail.com>
2024-09-22 22:34:25 +0700

    add env

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,12 +1,13 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
+import 'dotenv/config';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'admin',
-  database: 'alv.co',
+  host: process.env.PG_HOST,
+  port: Number(process.env.PORT),
+  username: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
   entities: ['dist/src/modules/**/*.entity.js'], // Path to entity
   migrations: ['dist/db/migrations/*.js'], // Path to migration
   synchronize: false,
